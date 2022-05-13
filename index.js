cdc6d0e073574f1a06a54da04104cf1f0411d2f4
Filipe <lipeqa@gmail>
2022-05-13 08:38:28 -0300

    atera arquivo css, mudando a posição do eleento a imagem ficou antes do texto

--- index.js
+++ index.js
@@ -2,16 +2,12 @@ let escolheDate = document.querySelector("#bntDate");
 let tituloApod = document.querySelector("#titulo");
 let textoApod = document.querySelector("#texto");
 let img = document.createElement("img")
-let divImg = document.querySelector(".conteiner");
+let divImg = document.querySelector("#conteiner");
 let date = document.querySelector("#date");
-let imgBack = document.querySelector("body")
 
 escolheDate.addEventListener("click",function(e){
     e.preventDefault();
-    //let date =  escolheDate.value;
-    //const key = Tq28PcxpwMdCgRkIdQfMQpp2VeQJ6EYJZilIxfDE;
     let req = `https://api.nasa.gov/planetary/apod?api_key=Tq28PcxpwMdCgRkIdQfMQpp2VeQJ6EYJZilIxfDE&date=${date.value}`;
-    
     //console.log(req);
     requisicao(req);
 })
@@ -23,9 +19,7 @@ function requisicao(urls){ // faz a requisição
         dataType: "json",
 
         success:function(result){
-            
-            console.log(result);
-            
+            //console.log(result);
             buscaObjeto(result);
         }
       });
@@ -35,11 +29,8 @@ function buscaObjeto(result){    //faz a busca das propriedades do personagem
      tituloApod.textContent =`${result.title} `
      textoApod.textContent = `${result.explanation}`
      img.src = `${result.url}`;
-     img.style.width = "100%";
-     img.style.height = "100%";
-     divImg.appendChild(img);
-
-     imgBack=style.backgroundColor = `${result.url}`;
-     
+     img.style.width = "50%";
+     //img.style.height = "80%";
+     divImg.appendChild(img);   
 }
 //https://api.nasa.gov/planetary/apod?api_key=Tq28PcxpwMdCgRkIdQfMQpp2VeQJ6EYJZilIxfDE
