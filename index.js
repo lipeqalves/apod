b5a74ace47a21e2708f22edb750485b777dbcf89
Filipe <lipeqa@gmail>
2022-05-17 23:30:51 -0300

    corrigi erro do video não carregar na pagina

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 let escolheDate = document.querySelector("#bntDate");
 let tituloApod = document.querySelector("#titulo");
 let textoApod = document.querySelector("#texto");
-let img = document.createElement("img")
+let img = document.createElement("img");
 let divImg = document.querySelector("#conteiner");
+let video = document.createElement("iframe");
 let date = document.querySelector("#date");
 
 escolheDate.addEventListener("click",function(e){
@@ -28,12 +29,22 @@ function requisicao(urls){ // faz a requisição
       });
 }
 function buscaObjeto(result){    //faz a busca das propriedades do personagem
-    
+    if(result.media_type == "image"){
+      video.style.display = "none";
+      img.style.display = "flex";
+      img.src = `${result.url}`;
+      img.style.width = "50%";
+      img.style.height = "50%";
+      divImg.appendChild(img); 
+    }else{
+      img.style.display = "none";
+      video.style.display = "flex";
+      video.src = `${result.url}`
+      video.style.width = "50%";
+      video.style.height = "50%";
+      divImg.appendChild(video)
+    }
      tituloApod.textContent =`${result.title} `
-     textoApod.textContent = `${result.explanation}`
-     img.src = `${result.url}`;
-     img.style.width = "50%";
-     img.style.height = "50%";
-     divImg.appendChild(img);   
+     textoApod.textContent = `${result.explanation}` 
 }
 //https://api.nasa.gov/planetary/apod?api_key=Tq28PcxpwMdCgRkIdQfMQpp2VeQJ6EYJZilIxfDE
